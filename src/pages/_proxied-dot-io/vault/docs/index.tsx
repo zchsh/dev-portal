8095f8c7f05fc0bd15e522b238c7fbbff112f742
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-14 13:35:05 -0400

    feat: get static props working on custom landing page

--- src/pages/_proxied-dot-io/vault/docs/index.tsx
+++ src/pages/_proxied-dot-io/vault/docs/index.tsx
@@ -1,5 +1,15 @@
 import { ReactElement } from 'react'
 import VaultIoLayout from 'layouts/_proxied-dot-io/vault'
+import { isVersionedDocsEnabled } from 'lib/env-checks'
+import productData from 'data/vault.json'
+
+// const product = { name: productData.name, slug: productData.slug }
+const basePath = 'docs'
+const navDataFile = `../data/${basePath}-nav-data.json`
+const localContentDir = `../content/${basePath}`
+const localPartialsDir = `../content/partials`
+const enableVersionedDocs = isVersionedDocsEnabled(productData.slug)
+// const additionalComponents = { Columns, Tag }
 
 /**
  * Note: we've switched from `/docs/[[...page]].tsx`, an "optional catch-all",
@@ -10,8 +20,46 @@ import VaultIoLayout from 'layouts/_proxied-dot-io/vault'
  * ref: https://nextjs.org/docs/routing/dynamic-routes#optional-catch-all-routes
  */
 
-function VaultDocsLandingPage(): ReactElement {
-  return <div>Hello Vault Landing page!</div>
+function VaultDocsLandingPage(props): ReactElement {
+  return (
+    <div>
+      Hello Vault Landing page!
+      <pre>
+        <code>{JSON.stringify({ props }, null, 2)}</code>
+      </pre>
+    </div>
+  )
+}
+
+// Imports below are used in getStatic functions only
+import { getStaticGenerationFunctions } from 'lib/_proxied-dot-io/get-static-generation-functions'
+import { GetStaticPropsContext, GetStaticPropsResult } from 'next'
+
+const {
+  getStaticProps: generatedGetStaticProps,
+} = getStaticGenerationFunctions(
+  enableVersionedDocs
+    ? {
+        strategy: 'remote',
+        basePath,
+        fallback: 'blocking',
+        product: productData.slug,
+      }
+    : {
+        strategy: 'fs',
+        localContentDir,
+        navDataFile,
+        localPartialsDir,
+        product: productData.slug,
+      }
+)
+
+// Export getStaticProps function
+export async function getStaticProps(
+  context: GetStaticPropsContext
+): Promise<GetStaticPropsResult<$TSFixMe>> {
+  const params = { page: [] }
+  return await generatedGetStaticProps({ ...context, params })
 }
 
 // Export view with layout
