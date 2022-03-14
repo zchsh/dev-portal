8ad34705657a8ba733e1c66f51fcaf65d3330cd1
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-14 13:04:15 -0400

    docs: add comment on optional vs plain catch-all routes

--- src/pages/_proxied-dot-io/vault/docs/index.tsx
+++ src/pages/_proxied-dot-io/vault/docs/index.tsx
@@ -1,6 +1,15 @@
 import { ReactElement } from 'react'
 import VaultIoLayout from 'layouts/_proxied-dot-io/vault'
 
+/**
+ * Note: we've switched from `/docs/[[...page]].tsx`, an "optional catch-all",
+ * to `/docs/[...page].tsx`, a "catch-all" route. As mentioned in the NextJS
+ * docs, the main difference is that the latter will not match the route
+ * without parameters - ie the landing page. This allows us to avoid
+ * conflicting page files.
+ * ref: https://nextjs.org/docs/routing/dynamic-routes#optional-catch-all-routes
+ */
+
 function VaultDocsLandingPage(): ReactElement {
   return <div>Hello Vault Landing page!</div>
 }
