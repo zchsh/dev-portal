6f7dbc83970912a88c212f59746440055f1999d4
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-14 13:48:14 -0400

    feat: stub in correct layout with custom content

--- src/pages/_proxied-dot-io/vault/docs/index.tsx
+++ src/pages/_proxied-dot-io/vault/docs/index.tsx
@@ -1,15 +1,18 @@
 import { ReactElement } from 'react'
 import VaultIoLayout from 'layouts/_proxied-dot-io/vault'
-import { isVersionedDocsEnabled } from 'lib/env-checks'
+import { DocsPageInner, DocsPageProps } from '@hashicorp/react-docs-page'
 import productData from 'data/vault.json'
+import { isVersionedDocsEnabled } from 'lib/env-checks'
+// Imports below are used in getStatic functions only
+import { getStaticGenerationFunctions } from 'lib/_proxied-dot-io/get-static-generation-functions'
+import { GetStaticProps } from 'next'
 
-// const product = { name: productData.name, slug: productData.slug }
+const product = { name: productData.name, slug: productData.slug }
 const basePath = 'docs'
 const navDataFile = `../data/${basePath}-nav-data.json`
 const localContentDir = `../content/${basePath}`
 const localPartialsDir = `../content/partials`
 const enableVersionedDocs = isVersionedDocsEnabled(productData.slug)
-// const additionalComponents = { Columns, Tag }
 
 /**
  * Note: we've switched from `/docs/[[...page]].tsx`, an "optional catch-all",
@@ -20,20 +23,32 @@ const enableVersionedDocs = isVersionedDocsEnabled(productData.slug)
  * ref: https://nextjs.org/docs/routing/dynamic-routes#optional-catch-all-routes
  */
 
-function VaultDocsLandingPage(props): ReactElement {
+function VaultDocsLandingPage({
+  frontMatter,
+  currentPath,
+  navData,
+  githubFileUrl,
+  versions,
+}: DocsPageProps['staticProps']): ReactElement {
   return (
-    <div>
-      Hello Vault Landing page!
-      <pre>
-        <code>{JSON.stringify({ props }, null, 2)}</code>
-      </pre>
-    </div>
+    <DocsPageInner
+      canonicalUrl={frontMatter.canonical_url}
+      description={frontMatter.description}
+      githubFileUrl={githubFileUrl}
+      navData={navData}
+      currentPath={currentPath}
+      pageTitle={frontMatter.page_title}
+      product={product}
+      showEditPage={false}
+      showVersionSelect={enableVersionedDocs}
+      baseRoute={basePath}
+      versions={versions}
+      algoliaConfig={productData.algoliaConfig}
+    >
+      <div>Hello Vault Landing page! Would put some custom content here.</div>
+    </DocsPageInner>
   )
 }
-
-// Imports below are used in getStatic functions only
-import { getStaticGenerationFunctions } from 'lib/_proxied-dot-io/get-static-generation-functions'
-import { GetStaticPropsContext, GetStaticPropsResult } from 'next'
 
 const {
   getStaticProps: generatedGetStaticProps,
@@ -55,12 +70,10 @@ const {
 )
 
 // Export getStaticProps function
-export async function getStaticProps(
-  context: GetStaticPropsContext
-): Promise<GetStaticPropsResult<$TSFixMe>> {
-  const params = { page: [] }
-  return await generatedGetStaticProps({ ...context, params })
+const getStaticProps: GetStaticProps = async (context) => {
+  return await generatedGetStaticProps({ ...context, params: { page: [] } })
 }
+export { getStaticProps }
 
 // Export view with layout
 VaultDocsLandingPage.layout = VaultIoLayout
