364bd4e8290f4c52abfb15137fb84e475beed020
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-15 09:21:49 -0400

    feat: stub in basic HTML content, stopgappy

--- src/pages/_proxied-dot-io/vault/docs/index.tsx
+++ src/pages/_proxied-dot-io/vault/docs/index.tsx
@@ -3,6 +3,10 @@ import VaultIoLayout from 'layouts/_proxied-dot-io/vault'
 import { DocsPageInner, DocsPageProps } from '@hashicorp/react-docs-page'
 import productData from 'data/vault.json'
 import { isVersionedDocsEnabled } from 'lib/env-checks'
+import {
+  generateSlug,
+  generateAriaLabel,
+} from '@hashicorp/platform-remark-plugins/util/generate-slug'
 // Imports below are used in getStatic functions only
 import { getStaticGenerationFunctions } from 'lib/_proxied-dot-io/get-static-generation-functions'
 import { GetStaticProps } from 'next'
@@ -45,8 +49,62 @@ function VaultDocsLandingPage({
       versions={versions}
       algoliaConfig={productData.algoliaConfig}
     >
-      <div>Hello Vault Landing page! Would put some custom content here.</div>
+      <DocsPageInnerCompatHeading level={1} title="Documentation" />
+      <p>
+        Welcome to Vault documentation! Vault is an identity-based secret and
+        encryption management system. This documentation covers the main
+        concepts of Vault, what problems it can solve, and contains a quick
+        start for using Vault.
+      </p>
+      <pre>
+        <code>[ FEATURE CARD PLACEHOLDER ]</code>
+      </pre>
+      <DocsPageInnerCompatHeading level={2} title="Use Cases" />
+      <pre>
+        <code>[ MULTI-LINK CARDS GRID PLACEHOLDER ]</code>
+      </pre>
+      <DocsPageInnerCompatHeading level={2} title="Developers" />
+      <pre>
+        <code>[ INTERACT-ABLE CARDS GRID PLACEHOLDER ]</code>
+      </pre>
     </DocsPageInner>
+  )
+}
+
+/**
+ * TODO
+ * This is a stopgap solution, because DocsPageInner automatically tries
+ * to inject a jump-to-section menu, and throws an error if there are
+ * any headings that do not have the __permalink-h attribute.
+ * I think we might have fixed the "throw error" part of the equation in
+ * a recent release, need to check that.
+ *
+ * Hot take here is that there isn't really a need for jump-to-section headings,
+ * so we should be fine to not render the __permalink-h stuff.
+ *
+ * Broader hot take is that it might be nice to modify DocsPageInner so that
+ * it does NOT automatically wrap content in the <Content /> component.
+ * The default export, DocsPage, would do the wrapping, so would be unaffected.
+ * This would open the possibility of moving away from cascading,
+ * inherited styles (which leak unintentionally into custom components,
+ * like Card), and towards more encapsulated styles.
+ */
+function DocsPageInnerCompatHeading({ level, title, children = title }) {
+  const Elem = `h${level}` as keyof JSX.IntrinsicElements
+  const slug = generateSlug(title, [])
+  return (
+    <Elem className={`g-type-display-${level + 1}`}>
+      <a
+        className="__permalink-h"
+        href={`#${slug}`}
+        aria-label={generateAriaLabel(title)}
+      >
+        »
+      </a>
+      {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
+      <a className="__target-h" id={slug} aria-hidden="true"></a>
+      {children}
+    </Elem>
   )
 }
 
