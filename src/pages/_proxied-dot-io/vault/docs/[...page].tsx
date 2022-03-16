259e06c491763dcbcaa8c497f86507c4c18465b9
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-16 11:45:58 -0400

    style: clarify comment, clean up with ternary

--- src/pages/_proxied-dot-io/vault/docs/[...page].tsx
+++ src/pages/_proxied-dot-io/vault/docs/[...page].tsx
@@ -54,17 +54,17 @@ const {
 /**
  * Note that we need to exclude the usually-included `/` empty path,
  * as otherwise, we'll have conflicting paths with our `/docs/index.tsx` file.
+ * (and NextJS will get mad since this is not an "optional catch-all" route).
  */
 export async function getStaticPaths(
   context: GetStaticPathsContext
 ): Promise<GetStaticPathsResult> {
   const { paths, ...restReturn } = await generatedGetStaticPaths(context)
-  const paramId = 'page'
   const pathsWithoutIndex = paths.filter((pathEntry) => {
-    if (typeof pathEntry == 'string') {
-      return pathEntry !== ''
-    }
-    const isIndexPath = pathEntry.params[paramId].length == 0
+    const isIndexPath =
+      typeof pathEntry == 'string'
+        ? pathEntry == ''
+        : pathEntry.params.page.length == 0
     return !isIndexPath
   })
   return { ...restReturn, paths: pathsWithoutIndex }
